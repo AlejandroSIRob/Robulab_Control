e9b0aaeb67c2e2294743c3bf02c319a635d53ccd
AlejandroSIRob <a.solar.iglesias@gmail.com>
2025-05-22 16:50:16 +0000

    control_gestos

---
+++ build/control_robulab/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/catkin_ws/src/control_robulab
+# Build directory: /root/catkin_ws/build/control_robulab
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/robulab_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/catkin_ws/src/robulab_control
+# Build directory: /root/catkin_ws/build/robulab_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
